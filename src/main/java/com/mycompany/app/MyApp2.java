3e7487c3d8981bb48367fc413ae685d877377f8e
Jagadesh Munta <jmunta@trustlogix.io>
2021-09-28 07:36:19 -0700

    feat: new api class added #11

---
+++ src/main/java/com/mycompany/app/MyApp2.java
@@ -0,0 +1,21 @@
+package com.mycompany.app;
+
+/**
+ * Sample my-app
+ *
+ */
+public class MyApp2 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!2" );
+    }
+
+    public static String sayHello2(String name) {
+        return "Hello World2, " + name;
+    }
+
+    public static String sayHelloAI(String name) {
+        return "Saying Hello AI, " + name;
+    }
+}
